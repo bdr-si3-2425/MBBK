2a9865afd129142a97e251bd427747a6e513fb3c
Haitam BENABAD <haitambenabad@gmail.com>
2025-02-05 10:22:16 +0100

    Final update REQUETE 1

--- Projet BDR/Requête1.sql
+++ Projet BDR/Requête1.sql
@@ -1,27 +1,23 @@
 --Trouver les amis d’amis d’un utilisateur donné : 
 
-    WITH amis_directs AS (
-        SELECT id_utilisateur2 AS ami
-        FROM "PROJET BDR".connexion
-        WHERE id_utilisateur1 = 1
-          AND EXISTS (
-            SELECT 1 FROM "PROJET BDR".connexion c
-            WHERE c.id_utilisateur1 = id_utilisateur2 AND c.id_utilisateur2 = id_utilisateur1
-          ) -- Vérifie que la connexion est mutuelle
-    ), 
-    amis_d_amis AS (
-        SELECT DISTINCT c2.id_utilisateur2 AS ami_potentiel
-        FROM "PROJET BDR".connexion c2
-        JOIN amis_directs a ON c2.id_utilisateur1 = a.ami
-        WHERE EXISTS (
-            SELECT 1 FROM "PROJET BDR".connexion c3
-            WHERE c3.id_utilisateur1 = c2.id_utilisateur2 AND c3.id_utilisateur2 = c2.id_utilisateur1
-        ) -- Vérifie que l'ami de l'ami est bien un ami mutuel
-          AND c2.id_utilisateur2 != 1 -- Évite l'auto-suggestion
-    ),
-    SELECT DISTINCT u.id_utilisateur, u.pseudo
-    FROM amis_d_amis
-    JOIN "PROJET BDR".utilisateur u ON amis_d_amis.ami_potentiel = u.id_utilisateur
-    WHERE NOT EXISTS (
-        SELECT 1 FROM amis_directs a WHERE a.ami = u.id_utilisateur
+    SELECT DISTINCT u2.id_utilisateur, u2.pseudo
+FROM "PROJET BDR".connexion c1
+JOIN "PROJET BDR".connexion c2 
+    ON c1.id_utilisateur2 = c2.id_utilisateur1
+JOIN "PROJET BDR".utilisateur u2 
+    ON c2.id_utilisateur2 = u2.id_utilisateur
+WHERE c1.id_utilisateur1 = 1  
+    AND c2.id_utilisateur2 != c1.id_utilisateur1
+    AND NOT EXISTS (
+        SELECT 1 FROM "PROJET BDR".connexion c3
+        WHERE c3.id_utilisateur1 = 1 AND c3.id_utilisateur2 = c2.id_utilisateur2
+    )
+    AND EXISTS (
+        SELECT 1 FROM "PROJET BDR".connexion c4
+        WHERE c4.id_utilisateur1 = c1.id_utilisateur2 AND c4.id_utilisateur2 = c1.id_utilisateur1
+    ) 
+    AND EXISTS (
+        SELECT 1 FROM "PROJET BDR".connexion c5
+        WHERE c5.id_utilisateur1 = c2.id_utilisateur2 AND c5.id_utilisateur2 = c2.id_utilisateur1
     );
+
